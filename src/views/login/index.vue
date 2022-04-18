3238265ce87fb1bb794b43ecfd419ae0502357e3
GUAQINGA <aaatea@outlook.com>
2022-04-18 16:56:10 +0800

    登录注册-布局

---
+++ src/views/login/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="login-container">
+    <van-nav-bar
+      class="page-nav-bar"
+      title="登录"
+    />
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="用户名"
+        placeholder="请输入手机号"
+      >
+        <i
+          slot="left-icon"
+          class="toutiao toutiao-shouji"
+        ></i>
+      </van-field>
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        placeholder="请输入验证码"
+        left-icon="smile-o"
+      >
+        <i
+          slot="left-icon"
+          class="toutiao toutiao-yanzhengma"
+        ></i>
+        <template #button>
+          <van-button
+            round
+            class="send-sms-btn"
+            size="small"
+            type="default"
+          >发送验证码</van-button>
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button
+          class="login-btn"
+          block
+          type="info"
+          native-type="submit"
+        >登录</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loginIndex',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit (values) {
+      console.log('submit', values)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  .toutiao {
+    font-size: 37px;
+  }
+
+  .send-sms-btn {
+    width: 152px;
+    height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+  }
+
+  .login-btn-wrap {
+    margin: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
+</style>
