e6908fe2851f51ad3462378ea1ce1da9f43df399
GUAQINGA <aaatea@outlook.com>
2022-04-18 21:47:01 +0800

    登陆注册-验证码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,64 +1,135 @@
 <template>
   <div class="login-container">
-    <van-nav-bar
-      class="page-nav-bar"
-      title="登录"
-    />
-    <van-form @submit="onSubmit">
+    <van-nav-bar class="page-nav-bar" title="登录" />
+    <van-form ref="loginForm" @submit="onSubmit">
       <van-field
-        v-model="username"
-        name="用户名"
+        v-model="user.mobile"
+        name="mobile"
         placeholder="请输入手机号"
+        :rules="userFormRules.mobile"
+        type="number"
+        maxlength="11"
       >
-        <i
-          slot="left-icon"
-          class="toutiao toutiao-shouji"
-        ></i>
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
       </van-field>
       <van-field
-        v-model="password"
-        type="password"
-        name="密码"
+        v-model="user.code"
+        name="code"
         placeholder="请输入验证码"
-        left-icon="smile-o"
+        :rules="userFormRules.code"
+        type="number"
+        maxlength="6"
       >
-        <i
-          slot="left-icon"
-          class="toutiao toutiao-yanzhengma"
-        ></i>
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
         <template #button>
+          <van-count-down
+            v-if="isCountDownShow"
+            :time="1000 * 10"
+            format="ss s"
+            @finish="isCountDownShow = false"
+          />
           <van-button
-            round
             class="send-sms-btn"
+            v-else
+            round
             size="small"
             type="default"
-          >发送验证码</van-button>
+            native-type="button"
+            @click="onSendSms"
+            >发送验证码</van-button
+          >
         </template>
       </van-field>
       <div class="login-btn-wrap">
-        <van-button
-          class="login-btn"
-          block
-          type="info"
-          native-type="submit"
-        >登录</van-button>
+        <van-button class="login-btn" block type="info" native-type="submit"
+          >登录</van-button
+        >
       </div>
     </van-form>
   </div>
 </template>
 
 <script>
+import { login, sendSms } from '@/api/user'
+
 export default {
   name: 'loginIndex',
   data () {
     return {
-      username: '',
-      password: ''
+      user: {
+        mobile: '',
+        code: ''
+      },
+      userFormRules: {
+        mobile: [
+          {
+            required: true,
+            message: '请输入手机号'
+          },
+          {
+            pattern: /^1[3|5|7|8]\d{9}$/,
+            message: '手机号格式错误'
+          }
+        ],
+        code: [
+          {
+            required: true,
+            message: '请输入验证码'
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: '验证码格式错误'
+          }
+        ]
+      },
+      isCountDownShow: false
     }
   },
   methods: {
-    onSubmit (values) {
-      console.log('submit', values)
+    async onSubmit (values) {
+      const user = this.user
+      console.log(user)
+
+      this.$toast.loading({
+        message: '登录中...',
+        forbidClick: true,
+        duration: 0
+      })
+
+      try {
+        const res = await login(user)
+        console.log('登录成功', res)
+        this.$toast.success('登录成功！')
+      } catch (error) {
+        if (error.response.status === 400) {
+          console.log('手机号或验证码错误！')
+          this.$toast.fail('手机号或验证码错误！')
+        } else {
+          console.log('登陆失败，请稍后再试', error)
+          this.$toast.fail('登录失败，请稍后再试！')
+        }
+      }
+    },
+    async onSendSms () {
+      try {
+        await this.$refs.loginForm.validate('mobile')
+      } catch (error) {
+        return console.log('验证失败', error)
+      }
+      this.isCountDownShow = true
+
+      try {
+        await sendSms(this.user.mobile)
+        this.$toast('发送成功')
+      } catch (error) {
+        this.isCountDownShow = false
+        if (error.response.status === 429) {
+          this.$toast('发送频繁，请稍后再试')
+        } else {
+          // this.$toast('发送失败，请稍后再试', error)
+        }
+        this.$toast('发送失败，请稍后再试', error)
+      }
     }
   }
 }
@@ -75,6 +146,7 @@ export default {
     height: 46px;
     background-color: #ededed;
     font-size: 22px;
+    line-height: 46px;
     color: #666;
   }
 
